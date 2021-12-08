e9f266e81a410c6341f551e00866ebdb9a3df337
BrianLecarpentier <brian.lecarpentier@teolia.fr>
2021-12-08 09:04:35 +0100

    [POK-2] Init flatlist

--- src/view/ListPokemons.js
+++ src/view/ListPokemons.js
@@ -1,21 +1,50 @@
 import React from 'react';
-import {FlatList, SafeAreaView, Text} from 'react-native';
+import {FlatList, Image, SafeAreaView, Text, View, StyleSheet} from 'react-native';
 import {useSelector} from 'react-redux';
 
 const ListPokemons = () => {
   const {pokemons} = useSelector(state => state.pokemonsReducer);
 
+  const pokemonCard = (index, item) => {
+      return (
+          <View style={styles.pokeCard}>
+              <Text ref={index}>{item.name}</Text>
+              <Image
+                  source={{
+                      uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${(index + 1)}.png`,
+                  }}
+                  style={styles.pokeImg}
+              />
+          </View>
+      )
+  }
+
   return (
       <SafeAreaView>
           <FlatList
               data={pokemons}
+              style={styles.container}
               renderItem={({item, index}) => {
-                  return <Text ref={index}>{item.name}</Text>;
+                  return (
+                      pokemonCard(index, item)
+              );
               }}
           />
       </SafeAreaView>
   )
 
 };
+
+const styles = StyleSheet.create({
+    container: {
+    },
+    pokeImg: {
+        width: 50,
+        height: 50,
+    },
+    pokeCard: {
+        width: '40%',
+    }
+})
 
 export default ListPokemons;
