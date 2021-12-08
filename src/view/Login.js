213569c56f1f1dae4913fa6958003342600a0353
teddy.villars <william.k97430@gmail.com>
2021-12-08 12:36:40 +0100

    [POK-6]-( style )

--- src/view/Login.js
+++ src/view/Login.js
@@ -6,40 +6,77 @@ import {
   StyleSheet,
   Text,
 } from 'react-native';
-import {Block, theme} from 'galio-framework';
+import {Block, Input, theme} from 'galio-framework';
 import {argonTheme, Images} from '../config';
-import {ButtonCustum} from '../components';
+import {ButtonCustum, InputCustum} from '../components';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const {width, height} = Dimensions.get('screen');
 
 const Login = () => {
   return (
     <Block flex middle>
-      <StatusBar hidden />
       <ImageBackground
         source={Images.RegisterBackground}
         style={{width, height, zIndex: 1}}>
         <Block safe flex middle>
           <Block style={styles.registerContainer}>
-            <Block flex={0.25} middle style={styles.socialConnect}>
+            <Block flex={1} middle>
+              <Text color="" size={12}>
+                Sign up with
+              </Text>
+              <Block row style={{marginTop: theme.SIZES.BASES}}>
+                <Block width={width * 0.8}>
+                  <InputCustum
+                    borderless
+                    placeholder="Email"
+                    icon={
+                      <Icon
+                        size={14}
+                        color={argonTheme.COLORS.ICON}
+                        name="user"
+                        family="AntDesign"
+                      />
+                    }
+                  />
+                </Block>
+              </Block>
+              <Block row style={{marginTop: theme.SIZES.BASES}}>
+                <Block width={width * 0.8}>
+                  <InputCustum
+                    borderless
+                    placeholder="Email"
+                    icon={
+                      <Icon
+                        size={16}
+                        color={argonTheme.COLORS.ICON}
+                        name="padlock-unlocked"
+                        family="ArgonExtra"
+                        style={styles.inputIcons}
+                      />
+                    }
+                  />
+                </Block>
+              </Block>
               <Text color="#8898AA" size={12}>
                 Sign up with
               </Text>
-              <Block row style={{marginTop: theme.SIZES.BASE}}>
+              <Block flex={0.5} middle>
                 <Text color="#8898AA" size={12}>
                   Sign up with
                 </Text>
-                <ButtonCustum>
-                  <Block row>
-                    {/*<Icon*/}
-                    {/*  name="facebook-square"*/}
-                    {/*  size={14}*/}
-                    {/*  color={'black'}*/}
-                    {/*  style={{marginTop: 2, marginRight: 5}}*/}
-                    {/*/>*/}
-                    <Text style={styles.socialTextButtons}>GITHUB</Text>
-                  </Block>
-                </ButtonCustum>
+                <Block row style={{marginTop: theme.SIZES.BASE}}>
+                  <ButtonCustum style={styles.socialButtons}>
+                    <Block row>
+                      <Text style={styles.socialTextButtons}>Google</Text>
+                    </Block>
+                  </ButtonCustum>
+                  <ButtonCustum style={styles.socialButtons}>
+                    <Block row>
+                      <Text style={styles.socialTextButtons}>FaceBook</Text>
+                    </Block>
+                  </ButtonCustum>
+                </Block>
               </Block>
             </Block>
           </Block>
@@ -53,13 +90,12 @@ const styles = StyleSheet.create({
   registerContainer: {
     width: width * 0.9,
     height: height * 0.875,
-    backgroundColor: '#49bef8',
-    opacity: 0.5,
+    backgroundColor: 'rgba(255,255,255,0.1)',
     borderRadius: 4,
     shadowColor: argonTheme.COLORS.BLACK,
     shadowOffset: {
       width: 0,
-      height: 4,
+      height: 2,
     },
     shadowRadius: 8,
     shadowOpacity: 0.1,
@@ -69,7 +105,7 @@ const styles = StyleSheet.create({
   socialConnect: {
     backgroundColor: argonTheme.COLORS.WHITE,
     borderBottomWidth: StyleSheet.hairlineWidth,
-    borderColor: '#8898AA',
+    borderColor: '#ffffff',
   },
   socialButtons: {
     width: 120,
@@ -98,7 +134,7 @@ const styles = StyleSheet.create({
     paddingBottom: 30,
   },
   createButton: {
-    width: width * 0.5,
+    width: width * 1.5,
     marginTop: 25,
   },
 });
